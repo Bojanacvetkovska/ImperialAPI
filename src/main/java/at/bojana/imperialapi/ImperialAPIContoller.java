4eabbdbc7dbbdc9764d2985d17f01fd52e63dbf1
LAPTOP-SQTE162T\bojan <bojanacvetko19@gmail.com>
2022-06-22 14:03:54 +0200

    Update ImperialApiController

--- src/main/java/at/bojana/imperialapi/ImperialAPIContoller.java
+++ src/main/java/at/bojana/imperialapi/ImperialAPIContoller.java
@@ -5,6 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ImperialAPIContoller {
     public static void main(String[]args){
+        class convert {
+            static double Conversion_Inches_to_cm(int Inches)
+            {
+                double centimeter;
+                centimeter  = 2.54 * Inches;
+                System.out.printf("Value in Centimeter is: %.2f \n", centimeter);
+                return 0;
+            }
+            public static void main(String args [])
+            {
+                int Inches = 20;
+                Conversion_Inches_to_cm(Inches);
+            }
+        }
 
     }
 }
