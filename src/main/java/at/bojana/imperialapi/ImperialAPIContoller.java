c68f377f62b73f505b1f5144e5b35a1e8c16fb6e
LAPTOP-SQTE162T\bojan <bojanacvetko19@gmail.com>
2022-06-22 13:51:03 +0200

    Add ImperialApiController

---
+++ src/main/java/at/bojana/imperialapi/ImperialAPIContoller.java
@@ -0,0 +1,10 @@
+package at.bojana.imperialapi;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ImperialAPIContoller {
+    public static void main(String[]args){
+
+    }
+}
